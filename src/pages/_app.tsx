f4c681b01576d061e5ba1d87a1c71ec4860b346b
Willian Lins Araújo <willian.araujo@anchieta.br>
2024-05-28 07:30:46 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { globalStyle } from "../styles/global";
+
+globalStyle()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
